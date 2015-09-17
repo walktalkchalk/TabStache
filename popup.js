c162456d5ea9302fe743ce7414ad786d7d2291ca
Dan Graham <dngrhm@hotmail.com>
2015-09-17 00:30:19 -0400

    Create named staches, retrieve/remove named staches

--- popup.js
+++ popup.js
@@ -1,9 +1,8 @@
 var tabStacheId = '0';
 
 document.addEventListener('DOMContentLoaded', function() {
-  var stashButton = document.getElementById('stashem');
-  var unstashButton = document.getElementById('unstashem');
-  var killstashButton = document.getElementById('killstash');
+  var new_stache = document.getElementById('new_stache')
+  var stache_list = document.getElementById('stache_list')
 
   chrome.bookmarks.search({'title':'TabStache_base'}, function(results) {
     if (results.length == 0) {
@@ -13,36 +12,51 @@ document.addEventListener('DOMContentLoaded', function() {
             'parentId': otherBookmarksID,
             'title': 'TabStache_base'
           }, function(node) {
-            stashButton.hide()
-            destashButton.show()
             tabStacheId = node.id
           })
       })
     } else {
       tabStacheId = results[0].id + ''
+      chrome.bookmarks.getChildren(tabStacheId, function(children) {
+        children.forEach(function(bookmark) {
+          var stache = document.createElement('button')
+          stache.setAttribute('value', bookmark.id)
+          stache.addEventListener('click', function() {
+            console.log(stache.value);
+            chrome.bookmarks.getChildren(stache.value, function(children) {
+               children.forEach(function(bookmark) {
+                 chrome.tabs.create({url: bookmark.url})
+               });
+               chrome.bookmarks.removeTree(stache.value);
+            });
+          });
+          stache.appendChild(document.createTextNode(bookmark.title))
+          stache_list.appendChild(stache)
+        });
+      });
     }
   });
 
-  stashButton.addEventListener('click', function() {
-    chrome.tabs.getAllInWindow(null, function(tabs) {
-      for (var i in tabs) {
-        chrome.bookmarks.create({
-              'parentId': tabStacheId,
-              'title': tabs[i].title,
-              'url': tabs[i].url})
-      }
-    })
+  new_stache.addEventListener('keyup', function (e) {
+    if ((e.keyCode == 13) && (new_stache.value)) {
+      console.log(new_stache.value);
+      chrome.bookmarks.create({
+        'parentId': tabStacheId,
+        'title': new_stache.value
+      }, function(node) {
+        chrome.tabs.getAllInWindow(null, function(tabs) {
+          chrome.tabs.create({});
+          for (var i in tabs) {
+            chrome.bookmarks.create({
+                  'parentId': node.id,
+                  'title': tabs[i].title,
+                  'url': tabs[i].url})
+            chrome.tabs.remove(tabs[i].id);
+          }
+        })
+      })
+      new_stache.value = "";
+    }
   });
 
-  unstashButton.addEventListener('click', function() {
-      chrome.bookmarks.getChildren(tabStacheId, function(children) {
-         children.forEach(function(bookmark) {
-           chrome.tabs.create({url: bookmark.url})
-         });
-      });
-  }, false);
-
-  killstashButton.addEventListener('click', function() {
-      chrome.bookmarks.removeTree(tabStacheId);
-  }, false);
 }, false);
