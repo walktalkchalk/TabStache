4956b117d3543cc3b25d6fb0ecdebb1c9a50ca71
Dan Graham <dngrhm@hotmail.com>
2015-09-27 20:43:42 -0400

    cleanup the javascript and change the stash list to a ul

--- popup.js
+++ popup.js
@@ -1,29 +1,32 @@
 var tabStacheId = '0';
 
 document.addEventListener('DOMContentLoaded', function() {
-  var new_stache = document.getElementById('new_stache')
-  var stache_list = document.getElementById('stache_list')
+  var new_stache = document.getElementById('new_stache');
+  var stache_list = document.getElementById('stache_list');
 
   chrome.bookmarks.search({'title':'TabStache_base'}, function(results) {
-    if (results.length == 0) {
+    if (results.length === 0) {
       chrome.bookmarks.getTree(function(tree){
           otherBookmarksID = tree[0].children[1].id;
           chrome.bookmarks.create({
             'parentId': otherBookmarksID,
             'title': 'TabStache_base'
           }, function(node) {
-            tabStacheId = node.id
-          })
-      })
+            tabStacheId = node.id;
+          });
+      });
     } else {
-      tabStacheId = results[0].id + ''
+      tabStacheId = results[0].id + '';
       chrome.bookmarks.getChildren(tabStacheId, function(children) {
         children.forEach(function(bookmark) {
-          var stache = document.createElement('button')
-          stache.setAttribute('value', bookmark.id)
+          var stache = document.createElement('button');
+          stache.setAttribute('value', bookmark.id);
+          stache.setAttribute('class', 'stache');
           stache.addEventListener('click', unload_stache);
-          stache.appendChild(document.createTextNode(bookmark.title))
-          stache_list.appendChild(stache)
+          stache.appendChild(document.createTextNode(bookmark.title));
+          var li = document.createElement('li');
+          li.appendChild(stache);
+          stache_list.appendChild(li);
         });
       });
     }
@@ -34,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
       chrome.bookmarks.create({
         'parentId': tabStacheId,
         'title': new_stache.value
-      }, load_stache)
+      }, load_stache);
       new_stache.value = "";
     }
   });
@@ -46,19 +49,19 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.bookmarks.create({
           'parentId': node.id,
           'title': tabs[i].title,
-          'url': tabs[i].url})
+          'url': tabs[i].url});
         chrome.tabs.remove(tabs[i].id);
       }
-    })
-  };
+    });
+  }
 
   function unload_stache() {
     _this = this;
     chrome.bookmarks.getChildren(this.value, function(children) {
        children.forEach(function(bookmark) {
-         chrome.tabs.create({url: bookmark.url})
+         chrome.tabs.create({url: bookmark.url});
        });
        chrome.bookmarks.removeTree(_this.value);
     });
-  };
+  }
 }, false);
